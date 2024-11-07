2ee6db31177ba303a6e4bd1bac0f40faf74c7585
Sean Allgaier <sallg001@odu.edu>
2024-11-07 13:43:44 -0500

    Updated backend server database information

--- course_advising.sql
+++ course_advising.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2024 at 05:38 AM
+-- Generation Time: Nov 07, 2024 at 07:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
